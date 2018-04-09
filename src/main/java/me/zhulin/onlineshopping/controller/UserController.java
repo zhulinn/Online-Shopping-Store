fa8ac863168741c552929dd27808864036893d54
Lin Zhu <julian.lin.zhu@gmail.com>
2018-04-09 05:36:27 -0400

    qqmusic

--- src/main/java/me/zhulin/onlineshopping/controller/UserController.java
+++ src/main/java/me/zhulin/onlineshopping/controller/UserController.java
@@ -1,23 +1,35 @@
 package me.zhulin.onlineshopping.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import me.zhulin.onlineshopping.entity.User;
 import me.zhulin.onlineshopping.exception.MyException;
 import me.zhulin.onlineshopping.repository.UserRepository;
 import me.zhulin.onlineshopping.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.security.Principal;
+import java.util.Iterator;
 
 /**
  * Created By Zhu Lin on 3/12/2018.
@@ -97,5 +109,29 @@ public class UserController {
     public String accessDeney(Model model) {
         model.addAttribute("msg", "Access denied!");
         return "/common/error";
+    }
+
+    @GetMapping("/music")
+    public String qqMusic(Model model) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("TransCode", "020337");
+        map.add("OpenId", "Test");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+
+        String url = "https://api.hibai.cn/api/index/index";
+
+        ResponseEntity<?> response = new RestTemplate().postForEntity(url, request, String.class);
+        String jsonString = (String) response.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(jsonString);
+        JsonNode songs = json.get("Body");
+
+        model.addAttribute("songs", songs);
+
+        return "music";
     }
 }
---
+++ src/main/resources/templates/music.ftl
@@ -0,0 +1,65 @@
+<html>
+<#include "partials/_header.ftl">
+<link rel="stylesheet" href="APlayer.min.css">
+
+<body>
+<div class="container ">
+    <h1 align="center" class="display-4 mb-5">QQ Music Top 50</h1>
+
+    <div style="margin: 25px auto; display: table;" >
+        <audio  onended="next()" id="audio" controls>
+            <source id="audioSource" src="" type="audio/mpeg">
+            Your browser does not support the audio format.
+        </audio>
+    </div>
+
+
+    <div id="aplayer"></div>
+
+    <table id="table" class="table table-striped text-center" style="width:100%;">
+        <thead>
+        <tr>
+            <th scope="col" >Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <#list 0..49 as i>
+        <tr>
+            <#--<th class="align-middle" scope="row">-->
+                <#--<img height="100px" src="">-->
+            <#--</th>-->
+            <td class="align-middle">${songs.get(i).get("title")}</td>
+            <td class="align-middle">${songs.get(i).get("author")}</td>
+            <td class="align-middle">
+                <a href="javascript:void(0)" id=${i} onclick="play(this)" data-src=${songs.get(i).get("url")} style="color: inherit"><i class="fas fa-play fa-lg"></i></a>
+            </td>
+
+        </tr>
+
+        </#list>
+        </tbody>
+    </table>
+
+
+
+</div>
+<#include "partials/_footer.ftl">
+<script src="APlayer.min.js"></script>
+
+<script>
+    function play(song) {
+        document.getElementById("audioSource").src = song.getAttribute('data-src');
+        var audio = document.getElementById('audio');
+        audio.load(); //call this to just preload the audio without playing
+        audio.play(); //call this to play the song right away
+    }
+    function next() {
+        var id = Math.floor((Math.random() * 50));
+        var song = document.getElementById(id);
+        play(song);
+    }
+</script>
+</body>
+</html>
